a5de6550c180a8919689f8da8842ea486695ba6c
homeostaceys <reyes.staceykyle@gmail.com>
2017-07-25 14:21:56 +0800

    itemdetail.html + some changes

--- marketplace/market/views.py
+++ marketplace/market/views.py
@@ -12,12 +12,7 @@ from .models import User, Post
 def index(request):
     latest_post_list = Post.objects.order_by('-pub_date')[:10]
     
-    #relative path siya, okay? :) not absolute bes
-    
     context = { 
-        # context is a dictionary
-        # to map the variables in the views and in the 
-        # template (index.html)
         'latest_posts': latest_post_list,
     }
     return render(request, 'market/homepage.html', context)
@@ -25,16 +20,12 @@ def index(request):
 def user(request):
     latest_post_list = Post.objects.order_by('-pub_date')[:10]
     
-    #relative path siya, okay? :) not absolute bes
-    
-    context = { 
-        # context is a dictionary
-        # to map the variables in the views and in the 
-        # template (index.html)
+    context = {
         'latest_posts': latest_post_list,
     }
     return render(request, 'market/userprofile.html', context)
 
 def postanitem(request):
+    
     return render(request, 'market/post_item.html')
     
