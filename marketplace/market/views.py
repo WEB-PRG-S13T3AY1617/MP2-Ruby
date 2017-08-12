d2d7da71bf4b65bcaa82a6e530681e5a93832dc3
homeostaceys <reyes.staceykyle@gmail.com>
2017-08-12 21:59:42 +0800

    minor fixes

--- marketplace/market/views.py
+++ marketplace/market/views.py
@@ -77,7 +77,7 @@ def user(request, user_id):
     }
     return render(request, 'market/userprofile.html', context)
 
-def postanitem(request, **kwargs):
+def postanitem(request):
     if request.user.is_authenticated():
         form = PostForm(request.POST or None, request.FILES or None)
         if form.is_valid():
@@ -96,27 +96,18 @@ def postanitem(request, **kwargs):
 def itemdetail(request, post_id):
     post =get_object_or_404(Post,pk=post_id)
     user2 = get_object_or_404(User,pk=post.user.id)
-    context = {
-            'user2':user2,
-            'post': post,
+    form = OfferForm(request.POST or None)
+    if form.is_valid():
+        that = form.save(commit=False)
+        that.user = request.user
+        that.save()
+        return redirect('market/itemdetail.html')
+    context = { 
+        'user2':user2,
+        'post': post,
+        'form': form,
     }
-    if request.user.is_authenticated():
-        form = OfferForm(request.POST or None, request.FILES or None)
-        if form.is_valid():
-            instance = form.save(commit=False)
-            instance.user = request.user
-            instance.save()
-            # message success
-            return redirect("/market/")
-        context = {
-            "form": form,
-        }
-        return render(request, "market/itemdetail.html", context)
-    else:
-        return render(request, 'market/itemdetail.html', context)
-
-
-
+    return render(request, 'market/itemdetail.html', context)
 
 def login_view(request):
     print(request.user.is_authenticated())
@@ -287,20 +278,20 @@ def searchcourse(request, post_course):
     }
     return render(request, 'market/homepage.html', context)
 
-def makeanoffer(request, **kwargs):
-    if request.user.is_authenticated():
-        form = OfferForm(request.POST or None, request.FILES or None)
-        if form.is_valid():
-            instance = form.save(commit=False)
-            instance.user = request.user
-            instance.save()
+#def makeanoffer(request, **kwargs):
+#    if request.user.is_authenticated():
+#        form = OfferForm(request.POST or None, request.FILES or None)
+#        if form.is_valid():
+#            instance = form.save(commit=False)
+#            instance.user = request.user
+#            instance.save()
             # message success
-            return redirect("/market/")
-        context = {
-            "form": form,
-        }
-        return render(request, "market/itemdetail.html", context)
-    else:
-        return index(request)
+#            return redirect("/market/")
+#        context = {
+#            "form": form,
+#        }
+#        return render(request, "market/itemdetail.html", context)
+#    else:
+#        return index(request)
 
 
